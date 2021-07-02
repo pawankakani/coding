69cebe912b748e7846269ba7d9d232a70cfeeff9
Pawan Kakani <pkakani@gmail.com>
2021-07-03 00:44:21 +0530

    assignment and arithmetic operators.

---
+++ python/arithmetic.py
@@ -0,0 +1,21 @@
+# assignment operator is used to store a value/result in a variable
+a = 5 ## <1>
+b = 2 ## <2>
+
+total = a + b           # addition
+diff = a - b            # subtraction
+quotient = a / b        # division
+product = a * b         # multiplication
+remainder = a % b       # mod
+int_quotient = a // b   # integer division
+exponent = a ** b       # exponentiation
+
+print("Value of a is :",a)
+print("Value of b is :",b)
+print("a + b = ", total)
+print("a - b = ", diff)
+print("a / b = ", quotient)
+print("a * b = ", product)
+print("a % b = ", remainder)
+print("a // b = ", int_quotient)
+print("a ** b = ", exponent)
