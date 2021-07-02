083c820b25353753d6040927a389b8032fdc6c69
Pawan Kakani <pkakani@gmail.com>
2021-07-02 15:38:57 +0530

    Initial commit. intro, data types.

---
+++ python/data_types.py
@@ -0,0 +1,19 @@
+month = "January"
+quantity = 120
+temperature = 45.6
+valid = True
+data = None
+
+# print values from variables
+print("Value of month :", month)
+print("Value of quantity :", quantity)
+print("Value of temperature :", temperature)
+print("Value of valid :", valid)
+print("Value of data :", data)
+
+# print data type of variables
+print("Type of month :", type(month))
+print("Type of quantity :", type(quantity))
+print("Type of temperature :", type(temperature))
+print("Type of valid :", type(valid))
+print("Type of data :", type(data))
