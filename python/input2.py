955b775594fe8f72b5fb31a2762b0dba96846ee3
Pawan Kakani <pkakani@gmail.com>
2021-07-15 12:50:13 +0530

    Updates for input handling.

---
+++ python/input2.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age :")) ## <1>
+temperature = float(input("Enter today's temperature :")) ## <2>
+
+print("Your age is :", age)
+print("Today's temperature is :", temperature)
