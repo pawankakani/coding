1a99e62911328823a32480b96ad8cb6154365ac2
Pawan Kakani <pkakani@gmail.com>
2021-07-19 19:39:04 +0530

    Added dictionary usage.

---
+++ python/dictionary.py
@@ -0,0 +1,16 @@
+# In below dictionary keys are of type string, and values are of type integer
+population = {'London': 8980000, 'Washington': 693000, 'Tokyo': 14000000, 'Frankfurt': 753000} ## <1>
+
+print(population['Washington']) # 693000 will be printed
+
+# In below dictionary, both keys and values are of type integer
+data = {10: 5000, 15: 3200, 25: 6000} ## <2>
+
+print(data[15]) # 3200 will be printed
+
+# In below dictionary, values are of type list
+stats = {'maths' : [120, 100, 210], 'physics' : [220, 140, 110]} ## <3>
+
+print(stats['physics']) # list [220, 140, 110] will be printed
+
+
