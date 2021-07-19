b001e477da07e482852c92b503d2ee9c0618693c
Pawan Kakani <pkakani@gmail.com>
2021-07-19 18:22:39 +0530

    Updated for tuples.

--- python/tuple.py
+++ python/tuple.py
@@ -4,11 +4,11 @@ daysOfWeek = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat') ## <1>
 # Another way to declare a tuple
 tenses = tuple(['Past', 'Present', 'Future'])
 
-print("Values in daysOfWeek are ", daysOfWeek)
-print("Values in tenses are ", tenses)
+print("Values in daysOfWeek are", daysOfWeek)
+print("Values in tenses are", tenses)
 
 # Accessing 4th element of tuple, note that index starts from zero
-print("4th day of week is ", daysOfWeek[3])
+print("4th day of week is", daysOfWeek[3])
 
 # Adding a new value to tuple will not work
 # daysOfWeek.append('test') ## <2>
