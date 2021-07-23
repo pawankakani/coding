545f130ea6c12a56df18466ddb16c4efc9f7c539
Pawan Kakani <pkakani@gmail.com>
2021-07-23 13:05:28 +0530

    decision making.

---
+++ python/if_else_1.py
@@ -0,0 +1,8 @@
+temperature = int(input("Enter temperature in degrees celcius : "))
+
+if temperature > 35:        ## <1>
+    print("It is very hot") ## <2>
+else:                       ## <3>
+    print("It is pleasant") ## <4>
+
+
