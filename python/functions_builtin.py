bb6019478a6ca65727547c51c12de03a7ed09abf
Pawan Kakani <pkakani@gmail.com>
2021-08-08 16:50:47 +0530

    Built-in functions.

---
+++ python/functions_builtin.py
@@ -0,0 +1,13 @@
+myList = [23, 65, 32, 12, 56, 85]
+print("Minimum number in list is", min(myList))
+print("Maximum number in list is", max(myList))
+print("Number of elements in list are", len(myList))
+
+
+city = "Amsterdam"
+print("Length of string is", len(city))
+
+s1 = "5"
+s2 = "45"
+print("Sum of two numbers is ", int(s1) + int(s2)) ## <1>
+
