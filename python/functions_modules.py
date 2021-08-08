fd7d2b293677cf3133fa4a3571858db35cd496b2
Pawan Kakani <pkakani@gmail.com>
2021-08-08 17:38:22 +0530

    Python module functions.

---
+++ python/functions_modules.py
@@ -0,0 +1,13 @@
+import math
+
+a = 9
+mylist = [10,20,30.7,40,50]
+
+print(math.sqrt(a)) # find square root
+print(math.sqrt(144))
+print(math.prod(mylist)) # product of all elements of list
+print(math.factorial(10)) # find factorial of a number
+print(math.ceil(3.2))
+print(math.floor(2.99))
+print(math.fsum(mylist)) # sum of all elements of list
+print(math.pow(2,3)) # find 2 to the power 3
